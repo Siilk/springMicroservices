82d50276ec67b38e9e3642cd9b7c6faa19715f6b
aerondir <aerondir@gmail.com>
2023-12-02 20:03:02 +1100

    one more microservice

--- api/src/main/java/se/magnus/api/core/price/Price.java
+++ api/src/main/java/se/magnus/api/core/price/Price.java
@@ -4,17 +4,20 @@ public class Price {
     private final int productId;
     private final float fullPrice;
     private final float discountPrice;
+    private final String serviceAddress;
 
     public Price() {
         this.productId = -1;
         this.fullPrice = 0;
         this.discountPrice = 0;
+        this.serviceAddress = "";
     }
 
-    public Price(int productId, float fullPrice, float discountPrice) {
+    public Price(int productId, float fullPrice, float discountPrice, String serviceAddress) {
         this.productId = productId;
         this.fullPrice = fullPrice;
         this.discountPrice = discountPrice;
+        this.serviceAddress = serviceAddress;
     }
 
     public int getProductId() {
@@ -27,5 +30,9 @@ public class Price {
 
     public float getDiscountPrice() {
         return discountPrice;
+    }
+
+    public String getServiceAddress() {
+        return serviceAddress;
     }
 }
